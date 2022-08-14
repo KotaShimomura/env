7fed8a2baac734839138ba1b361e482b2b0f4798
KotaShimomura <shimonarudo@gmail.com>
2022-08-14 23:17:06 +0900

    2hdd_mouts

---
+++ run_2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#
+TAG="kaggledockerimage"
+PROJECT_DIR="$(cd "$(dirname "${0}")/.." || exit; pwd)"
+
+docker run --gpus all \
+  -p 8888:8888 \
+  -it --rm \
+  -v "${PROJECT_DIR}:/workspace" \
+  -w "/workspace" \
+  "${TAG}" \
