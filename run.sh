61fc5f086a0a1eb2966f9cef0489e8b8881de9ef
KotaShimomura <shimonarudo@gmail.com>
2022-09-05 16:40:58 +0900

    add memory

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ PROJECT_DIR="$(cd "$(dirname "${0}")/.." || exit; pwd)"
 
 docker run --gpus all \
   -p 8888:8888 \
-  -it --rm \
+  -it --rm --ipc=host \
   -v "${PROJECT_DIR}:/workspace" \
   -w "/workspace" \
   "${TAG}" \
