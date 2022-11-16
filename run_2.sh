b136ea2b9cc2669bcace7a8104d631343cc64a11
KotaShimomura <shimonarudo@gmail.com>
2022-11-17 01:58:16 +0900

    set memory_size

--- run_2.sh
+++ run_2.sh
@@ -6,6 +6,7 @@ PROJECT_DIR="$(cd "$(dirname "${0}")/.." || exit; pwd)"
 docker run --gpus all \
   -p 8888:8888 \
   -it --rm \
+  --shm-size=48gb \
   -v "${PROJECT_DIR}:/workspace" \
   -w "/workspace" \
   "${TAG}" \
