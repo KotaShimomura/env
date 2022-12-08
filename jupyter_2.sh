3809c8b9c8b08ddb4f92f0d2e872be8e0dccc1ab
KotaShimomura <shimonarudo@gmail.com>
2022-12-08 18:51:11 +0900

    fix hdd2 mount

--- jupyter_2.sh
+++ jupyter_2.sh
@@ -5,6 +5,6 @@ PROJECT_DIR="$(cd "$(dirname "${0}")/.." || exit; pwd)"
 
 cd
 
-docker run --gpus all -v $PWD:/tmp/working -v /media/shimo/hdd/:/tmp/hdd -w=/tmp/working \
-    -p 8888:8888 --rm -it --shm-size=32gb kaggledockerimage jupyter notebook \
+docker run --gpus all -v $PWD:/tmp/working -v /media/shimo/hdd2/:/tmp/hdd -w=/tmp/working \
+    -p 8888:8888 --rm -it --shm-size=32gb "${TAG}" jupyter notebook \
     --no-browser --ip="0.0.0.0" --notebook-dir=/tmp/working --allow-roo
